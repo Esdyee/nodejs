148b362034f96dff37c2dccf7c97709515e29f3f
Je <joimh@naver.com>
2023-01-17 16:11:20 +0900

    - body-parser 설치 - post 전송

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true })); // 이해할 필요 X 이렇게 쓰라고 되어있을뿐
 
 app.listen(8080, () => {
 
@@ -15,4 +16,10 @@ app.get("/", (req, res) => {
 
 app.get("/write", (req, res) => {
 	res.sendFile(__dirname + "/write.html");
+});
+
+app.post("/add", (req, res) => {
+	res.send("success!");
+	// console.log(req.body.title)
+	console.log(req.body);
 });
