834d4237740a0b04aa75d24189c03550470532d0
Je <joimh@naver.com>
2023-01-18 13:36:00 +0900

    Database에 자료 저장

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true })); // 이해할 필요 X 이렇게 쓰라고 되어있을뿐
 
+
 const MongoClient = require("mongodb").MongoClient;
-MongoClient.connect("mongodb+srv://sd0809:1212123@nestcluster.axt8d.mongodb.net/?retryWrites=true&w=majority", (err, client) => {
+
+const dbInfo = "mongodb+srv://sd0809:1212123@nestcluster.axt8d.mongodb.net/?retryWrites=true&w=majority"
+
+let db;
+MongoClient.connect(dbInfo, function (err, client) {
+	console.log(err);
+	db = client.db('todoapp');
+
+	// 데이터 insert
 	app.listen(8080, () => {
 		console.log("listening on 8080");
 	});
 });
+
 
 
 app.get("/pet", (req, res) => {
@@ -23,6 +33,16 @@ app.get("/write", (req, res) => {
 });
 
 app.post("/add", (req, res) => {
+	const {title} = req.body;
+	const date = new Date();
+	insertData(title, date);
 	res.send("success!");
 	console.log(req.body);
 });
+
+// 데이터 insert
+function insertData(title, date) {
+	db.collection('post').insertOne({title: title, date: date}, (err, result) => {
+		console.log("저장완료");
+	});
+}
