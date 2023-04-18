179209f494d4e5ad22a14f444c45973eae01079e
Je <joimh@naver.com>
2023-04-18 14:29:33 +0900

    env 파일 제거

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 // const router = require('express').Router();
 // get router & db connection from server.js
 const router = require('express').Router();
+const ObjectId = require('mongodb').ObjectId;
 
 // get server.js of mongodb connection
 const server = require('../server.js');
@@ -12,9 +13,16 @@ router.get("/list", (req, res) => {
 	const getDb = server.getDb;
 
 	const db = getDb();
-
+	
 	// get data from db
 	db.collection("post").find().toArray((err, result) => {
+		//isMine property 추가
+		result.forEach((item) => {
+			const id = new ObjectId(item.author);
+			item.isMine = id.toString() === req.user._id.toString();
+		});
+		
+		// page 렌더링
 		res.render("list.ejs", { posts: result });
 	});
 });
